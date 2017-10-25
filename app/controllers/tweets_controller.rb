8223c6183cd0e7ec8e98f6e99ab93067b1f8d0f0
tkgs0109 <subtkgs@gmail.com>
2017-10-25 09:39:56 +0000

    yarn, jquery導入, view大枠作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,48 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @tweets = Tweet.all
+  end
+  
+  def new
+    @tweet = Tweet.new
+  end
+  
+  def create
+    @tweet = Tweet.create(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "ツイートを作成しました!"
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "ツイートを編集しました!"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "ツイートを削除しました!"
+  end
+  
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title, :content)
+  end
+  
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
